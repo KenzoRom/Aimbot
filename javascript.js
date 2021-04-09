2b85251777b11be09075feec9b2db10e04589f8b
KenzoRom <302808825@student.rocmondriaan.nl>
2021-04-09 07:34:57 +0200

    Commit2

---
+++ javascript.js
@@ -0,0 +1,19 @@
+//De convert variabele zorgt ervoor dat de ingevulde nummers van string naar int worden geconvert zodat we daar een berekening mee kunnen doen.//
+
+document.querySelector('#calculate').addEventListener("click", (e) => {
+    const playerXPos = document.querySelector('#xPlayer').value;
+        playerXPosConvert = parseInt(playerXPos);
+    const playerYPos = document.querySelector('#yPlayer').value;
+        playerYPosConvert = parseInt(playerYPos);
+    const playerStartDegrees = document.querySelector('#startDegrees').value;
+        playerStartDegreesConvert = parseInt(playerStartDegrees);
+    const enemyXPos = document.querySelector('#xEnemy').value;
+        enemyXPosConvert = parseInt(enemyXPos);
+    const enemyYPos = document.querySelector('#yEnemy').value;
+        enemyYPosConvert = parseInt(enemyYPos);
+            const perpendicular =  enemyXPosConvert - playerXPosConvert;
+            const base = enemyYPos - playerYPos;
+            const inverseTan = Math.atan(base / perpendicular) * (180 / Math.PI);
+            //const finalDegreeChange = playerStartDegreesConvert - tanInDegrees;
+            alert(inverseTan)
+})
